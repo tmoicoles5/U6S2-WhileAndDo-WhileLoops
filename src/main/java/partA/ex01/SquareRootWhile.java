8ad5ecb250168178d23d1df5a62a4db4512a3fba
tmoicoles5 <colestmoi@gmail.com>
2025-02-04 18:57:40 -0500

    changes
    push

--- src/main/java/partA/ex01/SquareRootWhile.java
+++ src/main/java/partA/ex01/SquareRootWhile.java
@@ -3,11 +3,20 @@ package partA.ex01;
 import java.util.Scanner;
 
 public class SquareRootWhile {
-    public static String squareRoot(){
-        String response = "";
-
-        return response;
+    public static String squareRoot() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        double x = scanner.nextDouble();
+        while (x <= 0) {
+            System.out.println("Input a number more than 0");
+            x = scanner.nextDouble();
+            if (x >=0){
+               x= Math.sqrt(x);
+            }
+        }
+        return String.valueOf(x);
     }
+
     public static void main(String args[]) {
         String outputSquareRoot = squareRoot();
         System.out.println(outputSquareRoot);
