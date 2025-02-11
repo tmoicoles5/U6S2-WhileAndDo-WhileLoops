4e1e25e10ab7d0c9d64b2ec3066de9782dd97cc3
tmoicoles5 <colestmoi@gmail.com>
2025-02-11 18:11:32 -0500

    push

--- src/main/java/partA/ex02/SumOfNums.java
+++ src/main/java/partA/ex02/SumOfNums.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SumOfNums {
     public static String sum(){
        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int i = 11;
       do {
@@ -13,10 +14,14 @@ public class SumOfNums {
       while (i<= 11);{
        i= scanner.nextInt();
         }
+            num --;
+        }  while (num>=0);
+        System.out.println();
 
 
 return "";
     }
+
     public static void main(String args[]) {
         String outputSum = sum();
         System.out.println(outputSum);
